b22801c1441b323acda8a88d165844497458a2ce
DJ Mulholland <131439684+DavidMulhollandUX@users.noreply.github.com>
2026-05-13 12:16:53 +1000

    Scroll to top on every route change

--- compass-static/src/App.tsx
+++ compass-static/src/App.tsx
@@ -1,14 +1,22 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import NavBar from './components/NavBar'
 import LandingPage from './pages/LandingPage'
 import AssessorPage from './pages/AssessorPage'
 import ReportsPage from './pages/ReportsPage'
 import ReportDetailPage from './pages/ReportDetailPage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <HashRouter>
       <div className="min-h-screen bg-background text-foreground">
+        <ScrollToTop />
         <NavBar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
